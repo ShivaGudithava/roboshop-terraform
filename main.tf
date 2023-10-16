8c8c08c9bdb4207bcb8f104625d02204dcf57086
LAPTOP-7BJSCJKS\ashok <ashok.csecr@gmail.com>
2023-10-16 17:46:39 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-module "vpc" {
+module "components" {
   source = "git::https://github.com/ShivaGudithava/tf-module-vpc.git"
 
-  for_each = var.vpc
+  for_each = var.components
   cidr = each.value["cidr"]
   subnets = each.value["subnets"]
 }
